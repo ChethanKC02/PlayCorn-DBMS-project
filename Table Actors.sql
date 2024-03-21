e3413f90182cc5b7c850015e9ff951b7b55dab58
ChethanKC02 <chethankc.369.369@gmail.com>
2024-03-21 11:28:55 +0530

    Initial commit

---
+++ Table Actors.sql
@@ -0,0 +1,72 @@
+-- Table: Actors
+
+-- | ActorID | FirstName    | LastName   |
+-- | ------- | ------------ | ---------- |
+-- |   1     | 'Matthew'    | 'McConaughey' |
+-- |   2     | 'Henry'      | 'Cavill'   |
+-- |   3     | 'Leonardo'   | 'DiCaprio' |
+-- |   4     | 'Rosa'       | 'Salazar'  |
+-- |   5     | 'Keanu'      | 'Reeves'   |
+-- |   6     | 'Yash'       | ''         |
+-- |   7     | 'Rishab'     | 'Shetty'   |
+-- |   8     | 'Prabhas'    | ''         |
+-- |   9     | 'Aamir'      | 'Khan'     |
+-- |   10    | 'Jessica'    | 'Chastain' |
+-- |   11    | 'Michael'    | 'Shannon'  |
+-- |   12    | 'Diane'      | 'Lane'     |
+-- |   13    | 'Kate'       | 'Winslet'  |
+-- |   14    | 'Keean'      | 'Johnson'  |
+-- |   15    | 'Mahershala' | 'Ali'      |
+-- |   16    | 'Viggo'      | 'Tarasov'  |
+-- |   17    | 'Srinidhi'   | 'Shetty'   |
+-- |   18    | 'Ramachandra'| 'Raju'     |
+-- |   19    | 'Anant'      | 'Nag'      |
+-- |   20    | 'Sanjay'     | 'Dutt'     |
+-- |   21    | 'Raj'        | 'B. Shetty'|
+-- |   22    | 'Prakash'    | 'Raj'      |
+-- |   23    | 'Anushka'    | 'Shetty'   |
+-- |   24    | 'Tamannaah'  | 'Bhatia'   |
+-- |   25    | 'Rana'       | 'Daggubati'|
+-- |   26    | 'Anushka'    | 'Sharma'   |
+-- |   27    | 'Sushant Singh'| 'Rajput' |
+-- |   28    | 'Saurabh'    | 'Shukla'   |
+-- |   29    | 'R. Madhavan'| ''         |
+-- |   30    | 'Sharman'    | 'Joshi'    |
+-- |   31    | 'Kareena'    | 'Kapoor'   |
+-- |   32    | 'Boman'      | 'Irani'    |
+-- |   33    | 'Omi'        | 'Vaidya'   |
+
+INSERT INTO Actors (ActorID, FirstName, LastName) VALUES
+(1, 'Matthew', 'McConaughey'),
+(2, 'Henry', 'Cavill'),
+(3, 'Leonardo', 'DiCaprio'),
+(4, 'Rosa', 'Salazar'),
+(5, 'Keanu', 'Reeves'),
+(6, 'Yash', ''),
+(7, 'Rishab', 'Shetty'),
+(8, 'Prabhas', ''),
+(9, 'Aamir', 'Khan'),
+(10, 'Jessica', 'Chastain'),
+(11, 'Michael', 'Shannon'),
+(12, 'Diane', 'Lane'),
+(13, 'Kate', 'Winslet'),
+(14, 'Keean', 'Johnson'),
+(15, 'Mahershala', 'Ali'),
+(16, 'Viggo', 'Tarasov'),
+(17, 'Srinidhi', 'Shetty'),
+(18, 'Ramachandra', 'Raju'),
+(19, 'Anant', 'Nag'),
+(20, 'Sanjay', 'Dutt'),
+(21, 'Raj', 'B. Shetty'),
+(22, 'Prakash', 'Raj'),
+(23, 'Anushka', 'Shetty'),
+(24, 'Tamannaah', 'Bhatia'),
+(25, 'Rana', 'Daggubati'),
+(26, 'Anushka', 'Sharma'),
+(27, 'Sushant Singh', 'Rajput'),
+(28, 'Saurabh', 'Shukla'),
+(29, 'R. Madhavan', ''),
+(30, 'Sharman', 'Joshi'),
+(31, 'Kareena', 'Kapoor'),
+(32, 'Boman', 'Irani'),
+(33, 'Omi', 'Vaidya');
