455e5667a3a41b46f0c05178e95a8a05d0b2de46
ChethanKC02 <chethankc.369.369@gmail.com>
2024-03-21 12:17:55 +0530

    Commit 21-03-2024 11:55

--- 02 Inserting Records/Movies.sql
+++ 02 Inserting Records/Movies.sql
@@ -1,6 +1,6 @@
 -- Before Insertion:
 -- +---------+--------------------------------+-------------+--------+---------+-------+-------------+---------+-----------+
--- | MovieID |             Title              | ReleaseDate | Genre  | Runtime | Rating| DirectorID | ActorID |  Language |
+-- | MovieID |             Title              | ReleaseDate | Genre  | Runtime | Rating| DirectorID  | ActorID |  Language |
 -- +---------+--------------------------------+-------------+--------+---------+-------+-------------+---------+-----------+
 -- |         |                                |             |        |         |       |             |         |           |
 -- +---------+--------------------------------+-------------+--------+---------+-------+-------------+---------+-----------+
@@ -22,7 +22,7 @@ INSERT INTO Movies VALUES
 -- After Insertion:
 -- After Insertion:
 -- +---------+--------------------------------+-------------+--------+---------+-------+-------------+---------+-----------+
--- | MovieID |             Title              | ReleaseDate | Genre  | Runtime | Rating| DirectorID | ActorID |  Language |
+-- | MovieID |             Title              | ReleaseDate | Genre  | Runtime | Rating| DirectorID  | ActorID |  Language |
 -- +---------+--------------------------------+-------------+--------+---------+-------+-------------+---------+-----------+
 -- |    1    |          Interstellar          | 2014-11-07  | Sci-fi |   169   |  8.7  |      1      |    1    |  English  |
 -- |    2    |         Man of Steel           | 2013-05-14  | Action |   143   |  7.1  |      1      |    2    |  English  |
@@ -31,9 +31,9 @@ INSERT INTO Movies VALUES
 -- |    5    |           John Wick            | 2014-12-14  | Action |   101   |  7.4  |      3      |    5    |  English  |
 -- |    6    |        K.G.F: Chapter 1        | 2017-12-21  | Action |   155   |  8.2  |      4      |    6    |  Kannada  |
 -- |    7    |        K.G.F: Chapter 2        | 2013-05-05  | Action |   168   |  8.3  |      4      |    6    |  Kannada  |
--- |    8    | Garuda Gamana Vrishabha Vahana| 2021-11-19  | Action |   161   |  8.3  |      5      |    7    |  Kannada  |
--- |    9    |   Baahubali: The Beginning    | 2015-07-10  | Action |   158   |  8.0  |      6      |    8    |   Telugu  |
--- |   10    |  Baahubali 2: The Conclusion  | 2017-04-28  | Action |   167   |  8.2  |      6      |    8    |   Telugu  |
+-- |    8    | Garuda Gamana Vrishabha Vahana | 2021-11-19  | Action |   161   |  8.3  |      5      |    7    |  Kannada  |
+-- |    9    |   Baahubali: The Beginning     | 2015-07-10  | Action |   158   |  8.0  |      6      |    8    |   Telugu  |
+-- |   10    |  Baahubali 2: The Conclusion   | 2017-04-28  | Action |   167   |  8.2  |      6      |    8    |   Telugu  |
 -- |   11    |               PK               | 2014-12-19  | Drama  |   151   |  8.1  |      7      |    9    |   Hindi   |
 -- |   12    |            3 Idiots            | 2009-12-25  | Comedy |   171   |  8.4  |      7      |    9    |   Hindi   |
 -- +---------+--------------------------------+-------------+--------+---------+-------+-------------+---------+-----------+
